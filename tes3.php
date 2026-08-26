c2f900b1584735bb3c17022b210fc84f66cf0791
ALI-NUR-RIZKI <alinurrizqi1@gmail.com>
2026-08-26 12:52:16 +0700

    tugas-kursus-php-php-lanjutan
    tugas-1-5

---
+++ tes3.php
@@ -0,0 +1,5 @@
+<?php
+echo "<script>
+    alert('Apakah anda bisa membuat ini?');
+</script>";
+?>
---
+++ tes4.php
@@ -0,0 +1,8 @@
+<?php
+
+$HurufBesar = "SAYA Sedang Belajar PHP dENGAN ViSual sTUDIO";
+
+// Mengubah seluruh string menjadi huruf kecil dan mencetaknya
+echo strtolower($HurufBesar);
+
+?>
