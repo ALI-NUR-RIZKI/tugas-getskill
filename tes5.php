1544ab682c196bcf5fa3b822ab567904e87ebbc2
ALI-NUR-RIZKI <alinurrizqi1@gmail.com>
2026-08-26 16:53:34 +0700

    Delete tes5.php

--- tes5.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Mengatur zona waktu agar sesuai dengan lokasi (misal: Asia/Jakarta)
-date_default_timezone_set("Asia/Jakarta");
-
-echo "<h2>Selamat Datang</h2>";
-
-// Format tanggal: d = Tanggal, M = Singkatan Bulan, y = 2 digit Tahun, H:i = Jam:Menit, a = am/pm
-echo "Hari Ini Tanggal : " . date("d M y H:i a");
-?>
